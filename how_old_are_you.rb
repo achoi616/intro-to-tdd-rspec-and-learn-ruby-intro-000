bd4ab87d43071292071c539d53cbed813a60c648
achoi616 <achoi616@gmail.com>
2020-06-05 02:07:40 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,5 +1,3 @@
-touch how_old_are_you.#!/usr/bin/env ruby -wKU
-
 require_relative './current_age_for_birth_year.rb'
 
 puts "What year were you born?"
@@ -9,5 +7,4 @@ users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old.
 
-def current_age_for_birth_year
-end
+
